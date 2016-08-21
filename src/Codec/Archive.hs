f19db9cec2b7c400f1c86cd9545ed502ac958e71
Thomas Tuegel <ttuegel@gmail.com>
2016-08-21 08:39:53 -0500

    add all Entry fields

--- src/Codec/Archive.hs
+++ src/Codec/Archive.hs
@@ -8,13 +8,15 @@ import Control.Exception ( Exception, bracket, throwIO )
 import Control.Monad ( when )
 import Data.ByteString ( ByteString )
 import qualified Data.ByteString as B
+import Data.Int ( Int64 )
 import Data.Typeable
 import Foreign.C.String
 import Foreign.C.Types ( CInt, CSize )
 import Foreign.Marshal.Alloc ( free, mallocBytes )
 import Foreign.Ptr ( Ptr, castPtr )
 import System.IO.Streams ( InputStream, makeInputStream, unRead )
-import System.Posix.Types ( Fd, FileMode )
+import System.Posix.Types
+       ( DeviceID, EpochTime, Fd, FileMode, GroupID, LinkCount, UserID )
 
 
 data ArchiveException = ArchiveException CInt
@@ -23,12 +25,28 @@ data ArchiveException = ArchiveException CInt
 instance Exception ArchiveException
 
 
-data Entry = Entry { hardlink :: FilePath
-                   , pathname :: FilePath
-                   , sourcepath :: FilePath
-                   , symlink :: FilePath
-                   , filetype :: FileMode
-                   , mode :: FileMode
+data TimeSpec = TimeSpec { sec :: !EpochTime
+                         , nsec :: !Int64
+                         }
+
+
+data Entry = Entry { hardlink :: !FilePath
+                   , pathname :: !FilePath
+                   , sourcepath :: !FilePath
+                   , symlink :: !FilePath
+                   , filetype :: !FileMode
+                   , mode :: !FileMode
+                   , gid :: !GroupID
+                   , uid :: !UserID
+                   , size :: !Int64
+                   , dev :: !DeviceID
+                   , ino64 :: !Int64
+                   , nlink :: !LinkCount
+                   , rdev :: !DeviceID
+                   , atime :: !TimeSpec
+                   , mtime :: !TimeSpec
+                   , ctime :: !TimeSpec
+                   , birthtime :: !TimeSpec
                    }
 
 getEntry :: Ptr ArchiveEntry -> IO Entry
@@ -39,6 +57,29 @@ getEntry p = do
   symlink <- archive_entry_symlink_w p >>= peekCWString
   mode <- archive_entry_mode p
   filetype <- archive_entry_filetype p
+  gid <- archive_entry_gid p
+  uid <- archive_entry_uid p
+  size <- archive_entry_size p
+  dev <- archive_entry_dev p
+  ino64 <- archive_entry_ino64 p
+  nlink <- archive_entry_nlink p
+  rdev <- archive_entry_rdev p
+  atime <- do
+    sec <- archive_entry_atime p
+    nsec <- fromIntegral <$> archive_entry_atime_nsec p
+    pure TimeSpec {..}
+  mtime <- do
+    sec <- archive_entry_mtime p
+    nsec <- fromIntegral <$> archive_entry_mtime_nsec p
+    pure TimeSpec {..}
+  ctime <- do
+    sec <- archive_entry_ctime p
+    nsec <- fromIntegral <$> archive_entry_ctime_nsec p
+    pure TimeSpec {..}
+  birthtime <- do
+    sec <- archive_entry_birthtime p
+    nsec <- fromIntegral <$> archive_entry_birthtime_nsec p
+    pure TimeSpec {..}
   pure Entry {..}
 
 
